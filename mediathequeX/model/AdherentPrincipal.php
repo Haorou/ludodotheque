77dd4ec86b33d546398f7bd07606d46ce453062f
admin <admin@BTSWIN7-99>
2018-12-18 11:50:33 +0100

    gaetan class adherent principal 11:50 18/12/2018

--- mediathequeX/model/AdherentPrincipal.php
+++ mediathequeX/model/AdherentPrincipal.php
@@ -1,29 +1,49 @@
 <?php
 abstract class AdherentPrincipal
 {
-    private $_renouveller_adhesions;
-    private $_chequeestDepose;
+    private $_renouveller_adhesions = false;
+    private $_chequeEstDepose = false;
     private $_coordonnees;
+    private $date_adhesions = array ();
+    
+    //-------------------------constructeur----------------------------------
     
     public function __construct(array $donnes)
     {
         $this->hydrate($donnes);
     }
     
+    //-----------------------------methode--------------------------------------
     
-    
-    public function setNom($nom)
-    {
-        $nom = (string)$nom;
-        
-        $this->_nom = $nom;
+    function isChequeDepose() {
+        return $this->_chequeEstDepose = true;
+    }
+    function isrenouveller_adhesions() {
+        return $this->_renouveller_adhesions = true;
     }
     
-    public function setPrenom($prenom)
+    //-----------------------------getter and setter---------------------------------------------
+    
+    
+    public function setCoordonnees($coordonnees)
     {
-        $prenom = (string)$prenom;
+        $coordonnees = (string)$coordonnees;
         
-        $this->_prenom = $prenom;
+        $this->_coordonnees = $coordonnees;
+    }
+    
+    public function setChequeEstDepose($chequeEstDepose)
+    {
+        $chequeEstDepose = (bool)$chequeEstDepose;
+        
+        $this->_prenom = $chequeEstDepose;
+    }
+    
+    public function setRenouvellerAdhesion($renouvellerAdhesion)
+    {
+        $renouvellerAdhesion = (bool)$renouvellerAdhesion;
+        
+        $this->_renouveller_adhesions = $renouvellerAdhesion;
     }
 }
 ?>
