fea679552fa913ab9e240092c970a716bc5883a1
JulienB <julien.brulle@gmail.com>
2018-12-18 11:32:52 +0100

    add managerPDO

---
+++ mediathequeX/model/Emprunt.php
@@ -0,0 +1,44 @@
+<?php
+class Emprunt
+{
+    private $_dateEmprunt;
+    private $_estEnRetard;
+    
+    public function __construct(array $donnes)
+    {
+        $this->hydrate($donnes);
+    }
+    
+    /*
+     public function hydrate(array $donnes)
+     {
+         foreach($donnes as $cles => $values)
+         {
+         $method = "set" .ucfirst($cles);
+         
+             if(method_exists($this,$method))
+             {
+             $this->$method($values);
+             }
+         }
+     }
+     */
+    
+    public function dateEmprunt() { return $this->_dateEmprunt; }
+    
+    public function estEnRetard() { return $this->_estEnRetard; }
+
+    public function setDateEmprunt($date)
+    {
+        // Verifier si DateEmprunt est valide (if $date === ???)
+        $this->_dateEmprunt = $date;
+    }
+    
+    public function setEstEnRetard($estEndRetard)
+    {
+        $estEndRetard = (bool)$estEndRetard;
+        
+        $this->_estEnRetard = $estEndRetard;
+    }
+    
+}
---
+++ mediathequeX/model/ManagerPDO.php
@@ -0,0 +1,14 @@
+<?php
+
+class ManagerPDO
+{
+    function dbConnect()
+    {
+        // typeSQL : hote de la base de données (ici local) ; nom base de données ; 
+        // type charset ; nom utilisateur ; mot de passe
+        $db = new PDO("mysql:host=localhost;dbname=combats;charset=utf8","root","");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // On émet une alerte à chaque fois qu'une requête a échoué.
+        return $db;
+    }
+    
+}
