17315519f66fc34881eb51c0e8efeba60f2c7541
admin <admin@172.16.120.46>
2019-01-09 14:03:02 +0100

    dôjazebdzbdzudbui

--- mediathequeX/model/PDO/EmpruntManager.php
+++ mediathequeX/model/PDO/EmpruntManager.php
@@ -19,73 +19,50 @@ require_once("model/PDO/ManagerPDO.php");
         public function createEmprunt(Emprunt $empruntArticle)
         {
             $addEmpruntArticle = $this->_db->prepare("INSERT INTO emprunt(id_article, id_adherent, date_emprunt, date_de_retour_effectif, commentaire)
-                                            VALUES(:id_article, :id_adherent, :date_emprunt, :date_de_retour_effectif, :commentaire)");
+                                            VALUES(:id_article, :id_adherent, :date_emprunt, :commentaire)");
             $addEmpruntArticle->execute(array(
                 "id_article" => $empruntArticle->article()->id(),
                 "id_adherent" => $empruntArticle->adherent()->id(),
                 "date_emprunt" => $empruntArticle->date_emprunt(),
-                "date_de_retour_effectif" => $empruntArticle->date_de_retour_effectif(),
                 "commentaire" => $empruntArticle->commentaire()
             ));
         }
         
-        public function deleteEmprunt(Emprunt $perso)
+        public function updateEmprunt(Emprunt $empruntArticle)
         {
-            $deleteAyantDroit = $this->_db->prepare("DELETE FROM ayantdroit
-                                        WHERE id = :id)");
-            $deleteAyantDroit->execute(array(
-                "id" => $perso->id()));
-            
-            $deletePersonne = $this->_db->prepare("DELETE FROM personne
-                                        WHERE id = :id)");
-            $deletePersonne->execute(array(
-                "id" => $perso->id()));
+            $updateEmprunt = $this->_db->prepare("UPDATE emprunt SET
+                                                    date_de_retour_effectif = :date_de_retour_effectif,
+                                                    commentaire = :commentaire WHERE 
+                                                    id_article = id_article AND date_emprunt = :date_emprunt )");
+            $updateEmprunt->execute(array(
+                "date_de_retour_effectif" => $empruntArticle->id(),
+                "commentaire" => $empruntArticle->commentaire(),
+                "id_article" => $empruntArticle->article()->id(),
+                "date_emprunt" => $empruntArticle->date_emprunt()
+            ));
         }
 
         
-        public function readAyantDroit(Adherent $perso)
+        public function readEmpruntsAdherent(Adherent $perso)
         {
-            $q = $this->_db->prepare('SELECT * FROM ayantdroit
-                                          INNER JOIN adherent ON adherent.id = ayantdroit.id_adherent
-                                          INNER JOIN personne ON ayantdroit.id_personne = personne.id WHERE id = :id');
-            $q->execute([':id' => $perso->id()]);
-            $donnees = $q->fetch(PDO::FETCH_ASSOC);
+            $requeteEmprunts = $this->_db->prepare('SELECT * FROM adhrent
+                                      INNER JOIN emprunt ON emprunt.id_adherent = adherent.id_personne
+                                      WHERE emprunt.id_adherent = :id');
+            $requeteEmprunts->execute([':id' => $perso->id()]);
             
-            return new AyantDroit($donnees);
-        }
-        
-        
-        public function ayantDroitExists($info)
-        {
-            if (is_int($info)) // On veut voir si tel ayantdroit existe ayant pour id $info existe.
+            $listeEmprunts = [];
+            
+            while($requeteEmprunt = $requeteEmprunts->fetch())
             {
-                return (bool) $this->_db->query('SELECT COUNT(*) FROM ayantdroit WHERE id_personne = '.$info)->fetchColumn();
+                $listeEmprunts = new Emprunt($requeteEmprunt);
             }
-            // Sinon, c'est qu'on veut vérifier que le nom existe ou pas.
-            $q = $this->_db->prepare('SELECT COUNT(*) FROM personne INNER JOIN ayantdroit ON ayantdroit.id_personne = personne.id WHERE nom = :nom');
-            $q->execute([':nom' => $info]);
             
-            return (bool) $q->fetchColumn();
-        }
-
-        
-        public function readAllAyantDroit()
-        {
-            $selectPerso = $this->_db->query("SELECT * FROM ayantdroit
-                                              INNER JOIN adherent ON adherent.id = ayantdroit.id_adherent
-                                              INNER JOIN ayantdroit ON ayantdroit.id_personne = personne.id");
-            $listeDePersonnages = [];
-            
-            while($perso = $selectPerso->fetch())
-            {
-                $listeDePersonnages[] = new AyantDroit($perso);
-            }
-            return $listeDePersonnages;
+            return $listeEmprunts;
         }
         
-        public function countAyantDroit()
+        public function countEmprunt()
         {
-            return $this->_db->query("SELECT COUNT(*) FROM ayantdroit")->fetchColumn();
+            return $this->_db->query("SELECT COUNT(*) FROM emprunt")->fetchColumn();
         }
         
     }
