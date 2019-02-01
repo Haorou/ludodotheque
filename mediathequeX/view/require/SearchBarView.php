464e9abdff37601ece1cba084f421ffa5d9d084a
JulienB <julien.brulle@gmail.com>
2019-02-01 17:14:23 +0100

    aaa

--- mediathequeX/view/require/SearchBarView.php
+++ mediathequeX/view/require/SearchBarView.php
@@ -15,8 +15,7 @@ elseif($GLOBALS["isActiveAlerte"] == TRUE)
 }
 ?>
 
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-    <div class="container" ><br/>
+	<div class="container" ><br/>
         <div class="row justify-content-center">
             <div class="col-12 col-md-10 col-lg-8">
                 <form class="">
@@ -45,11 +44,7 @@ elseif($GLOBALS["isActiveAlerte"] == TRUE)
     
     <div class="container" style="margin-top: 10px">
         <div class="row"  >
-            <div class="col-md-10" > <!-- style="background-color: red"  UTILE A METTRE POUR VOIR LA TAILLE DES CHAMPS -->
-            
-            <!-- IL FAUT PEUPLER CECI EN FONCTION D'UNE REQUETE INITAL GENERAL "SELECT * FROM ... " -->
-	        <!-- PUIS PRECISER LA REQUETE EN FONCTION DES MOTS TAPPER DANS LE CHAMPS DE RECHERCE "SELECT * FROM ... WHERE = "champs de recherche" " -->
-	        <!-- POUR ACTUALISER : UTILISATION JAVASCRIPT -->
+            <div class="col-md-10" > 
             
               		<?php if ($GLOBALS["isActiveAdherent"] == TRUE) { ?>
                   		<form action="?action=gestionAdherents" method="post">
@@ -112,10 +107,7 @@ elseif($GLOBALS["isActiveAlerte"] == TRUE)
                       
                     </select>
 
-    			<?php if ($GLOBALS["isActiveAdherent"] == TRUE) { ?>
-    			
-    			
-    			<?php } else if ($GLOBALS["isActiveJeu"] == TRUE) {?>
+    			<?php if ($GLOBALS["isActiveJeu"] == TRUE) {?>
     			
                 <div class="row">                    
                     <div class="col-md-12" style="text-align:center ; margin-top :20px">
@@ -124,14 +116,7 @@ elseif($GLOBALS["isActiveAlerte"] == TRUE)
                     </div>
                 </div>
     			
-    			<?php } else { ?>
-                
-                <div class="row">
-                    <div class="col-md-6">.col-md-6</div>
-                    <div class="col-md-6">.col-md-6</div>
-                </div>
-                
-                <?php }?>
+    			<?php }?>
                 
             </div>
             
