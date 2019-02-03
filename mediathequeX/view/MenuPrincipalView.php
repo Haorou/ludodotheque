839da697c674d07dceaa1312f9be70b5196da3d2
admin <admin@BTSWIN7-99>
2019-02-03 16:28:32 +0100

    Ebauche PAGE ACCUEIL

--- mediathequeX/view/MenuPrincipalView.php
+++ mediathequeX/view/MenuPrincipalView.php
@@ -2,16 +2,46 @@
 <html>
 
 <head>
-    <title>Mediathèque : MenuPrincipal</title>
+<title>Mediathèque : MenuPrincipal</title>
     
  	<?php require("view/require/HeadReferenceView.php");?>
  
  </head>
 
-    <body> 
+<body> 
 		<?php require("view/require/HeaderView.php");?>
 		
-		<h1 class="form-heading" style="text-align: center">Bienvenue gestionnaire / administrateur</h1>
+		<h1 class="form-heading" style="text-align: center">Accueil Ludothèque</h1>
+
+			<div class="jumbotron">
+				<h1 class="display-4">Bienvenue Gestionnaire!</h1>
+				<p class="lead">
+				<div>
+								<strong>La ludo, c'est un lieu :</strong>
+								<ul>
+								<li>Où l'on emprunte des jeux,</li>
+								<li>Où l'on expérimente des jeux que l'on n'a pas chez soi, </li>
+								<li>Un lieu où on peut prendre le temps de jouer,</li>
+								<li>D'éveil pour les tous petits,</li>
+								<li>De convivialité,</li>
+								<li>De rencontres.</li>
+								</ul>
+								<strong>C'est aussi : </strong>
+								<ul>
+								<li>Des conseils éclairés,</li>
+								<li>Des explications sur les règles des jeux,</li>
+								<li>Des animations ludiques pendant les temps forts de l'année.</li>
+								</ul>
+								<br>
+								<strong>Vous pourrez gérer les adhérents, les jeux et les emprunts de façon très intuitive.</strong>
+				</div>				
+				</p>
+				<hr class="my-4">
+				<p>Pour plus d'informations veuillez consulter le manuel utilisateur :</p>
+				<p class="lead">
+					<a class="btn btn-primary btn-lg" href="#" role="button">Manuel Utilisateur</a>
+				</p>
+			</div>
 
 		<?php require("view/require/FooterView.php");?>
 		
